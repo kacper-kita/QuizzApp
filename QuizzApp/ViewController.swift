28d2457662d63f38aaec016944f7b36ac9a5ff7e
Kacper Kita <kkita.contact@gmail.com>
2022-02-02 19:48:45 +0100

    Initial Commit

---
+++ QuizzApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizzApp
+//
+//  Created by Kacper Kita on 02/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
