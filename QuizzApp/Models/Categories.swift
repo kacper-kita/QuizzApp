d505469acd4622d92f71bd78f668ac504d9be5e8
Kacper Kita <kkita.contact@gmail.com>
2022-02-12 10:32:29 +0100

    Create model for categiories

---
+++ QuizzApp/Models/Categories.swift
@@ -0,0 +1,21 @@
+//
+//  Categories.swift
+//  QuizzApp
+//
+//  Created by Kacper Kita on 12/02/2022.
+//
+
+import Foundation
+
+struct Category: Decodable {
+    let id: Int
+    let name: String
+    let image: String?
+    let category_url: String
+    let color: String?
+    let description: String?
+}
+
+struct Categories: Decodable {
+    let category: [Category]?
+}
